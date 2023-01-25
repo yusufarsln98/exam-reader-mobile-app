cec803516e5d60d08a5367fe077663cfd800a2ab
Yusuf Arslan <yusufarsln98@outlook.com>
2023-01-25 17:48:50 +0300

    Exam Reader App with Expo React Native

---
+++ exam-reader/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.js to dsfsad working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // text color is red
+  },
+  text: {
+    color: 'red',
+  }
+});
