5b40f8ff3efba48f05460827e4aa5e4ed569b693
Serhat SARI <serhhatsari@gmail.com>
2023-01-27 23:45:24 +0300

    added auth service

--- exam-reader/services/authService.js
+++ exam-reader/services/authService.js
@@ -1,20 +1,28 @@
 import { app } from '../firebaseConfig';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const db = getFirestore(app);
 const auth = getAuth(app);
 
-function signup(email, password) {
+function signup(name, surname, email, password) {
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // Signed in 
             const user = userCredential.user;
-            console.log(user);
-            // ...
+            db.collection("users").doc(user.uid).set({
+                id: user.uid,
+                name: name,
+                surname: surname,
+                email: user.email,
+                classes: []
+            })
+
+            return db.collection("users").doc(user.uid).get();
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            // ..
+            console.log(errorCode, errorMessage);
         });
 }
 
@@ -22,13 +30,15 @@ function signup(email, password) {
 function login(email, password) {
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // Signed in 
             const user = userCredential.user;
-            // ...
+            console.log(db.collection("users").doc(user.uid).get());
+            return db.collection("users").doc(user.uid).get();
+
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
         });
 
 }
