d75f2b998a8cabff370a95e400f9453cfa52f0be
Yusuf Arslan <yusufarsln98@outlook.com>
2023-01-29 04:27:31 +0300

    Logo and icon changes

---
+++ exam-reader/components/icons.js
@@ -0,0 +1,22 @@
+import { Feather } from '@expo/vector-icons';
+import React from 'react';
+
+export const IconMail = (props) => {
+    return <Feather name="mail" size={24} color="black" {...props} />;
+}
+
+export const IconLock = (props) => {
+    return <Feather name="lock" size={24} color="black" {...props} />;
+}
+
+export const IconEye = (props) => {
+    return <Feather name="eye" size={24} color="black" {...props} />;
+}
+
+export const IconEyeOff = (props) => {
+    return <Feather name="eye-off" size={24} color="black" {...props} />;
+}
+
+export const IconUser = (props) => {
+    return <Feather name="user" size={24} color="black" {...props} />;
+}
