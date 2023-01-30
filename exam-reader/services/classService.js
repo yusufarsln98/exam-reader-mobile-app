38a04dd57f6ea5eaeac6f40fba454f3e71154ac7
Yusuf Arslan <yusufarsln98@outlook.com>
2023-01-30 23:35:57 +0300

    uid

--- exam-reader/services/classService.js
+++ exam-reader/services/classService.js
@@ -1,9 +1,15 @@
 import { app } from '../firebaseConfig';
 import { getFirestore } from "firebase/firestore";
+import { AppContext } from '../App';
+import React from 'react';
 
 const db = getFirestore(app);
 
+
+
 function addClass(user, className) {
+    const uid = React.useContext(AppContext).value.userData.id;
+    
     return db.collection("users").doc(user.id).update({
         classes: firebase.firestore.FieldValue.arrayUnion(className)
     });
