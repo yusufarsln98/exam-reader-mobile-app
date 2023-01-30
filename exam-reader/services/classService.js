8813b608b2175d770874451e05e85e448a13bcf5
Yusuf Arslan <yusufarsln98@outlook.com>
2023-01-30 23:37:21 +0300

    delete context api in service

--- exam-reader/services/classService.js
+++ exam-reader/services/classService.js
@@ -1,15 +1,9 @@
 import { app } from '../firebaseConfig';
 import { getFirestore } from "firebase/firestore";
-import { AppContext } from '../App';
-import React from 'react';
 
 const db = getFirestore(app);
 
-
-
 function addClass(user, className) {
-    const uid = React.useContext(AppContext).value.userData.id;
-    
     return db.collection("users").doc(user.id).update({
         classes: firebase.firestore.FieldValue.arrayUnion(className)
     });
