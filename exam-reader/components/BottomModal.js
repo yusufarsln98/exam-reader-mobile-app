c644f192ca6a589a092d94db225f871d74255a20
Yusuf Arslan <yusufarsln98@outlook.com>
2023-03-03 23:02:07 +0300

    bottom sheet test

--- exam-reader/components/BottomModal.js
+++ exam-reader/components/BottomModal.js
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import Modal from 'react-native-modal'
 import { COLORS, DIMENSIONS } from '../constants';
 import { globalStyles } from "../screens/styles";
-
+import BottomSheet from '@gorhom/bottom-sheet';
+import { Button } from '@rneui/base';
 
 
 // modal is closed by swiping down or tapping outside of the modal
 // bottom half modal
 
 function BottomModal() {
-  const [isVisible, setIsVisible] = React.useState(false);
+  const bottomSheetRef = useRef(null);
+  const [index, setIndex] = useState(0);
+  const snapPoints = useMemo(() => ['5%', '30%'], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index) => {
+    setIndex(index);
+    console.log('handleSheetChanges', index);
+  }, []);
+
+
+
 
   return (
     <View style={styles.container}>
-      <View style={styles.modalOpener} >
-        <View
-          style={styles.line}
-        >
-
-        </View>
-      </View>
+      <Button title="Open Bottom Sheet" onPress={() => bottomSheetRef.current.expand()} />
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+        detached={true}
+        enablePanDownToClose={true}
+      >
+        <Button title="Close Bottom Sheet" onPress={() => bottomSheetRef.current.collapse()} />
+      </BottomSheet>
     </View >
   )
 }
@@ -30,34 +46,7 @@ export default BottomModal;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: COLORS.bgColor,
   },
-  modalOpener: {
-    width: '100%',
-    height: 24,
-    backgroundColor: COLORS.snow,
-    borderTopEndRadius: 10,
-    borderTopStartRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 0,
-    margin: 'auto',
-    // add shadow to the top of the modal
-    shadowColor: COLORS.black,
-    shadowOffset: {
-      width: 0,
-      height: -2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  line: {
-    width: '20%',
-    height: 3,
-    backgroundColor: COLORS.primary,
-    borderRadius: 5,
-  },
+
 });
